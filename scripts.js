73c03de01489fc7a77d88b1cfd0688f40fa8b01e
Ismael <hola@ismaelbenito.com>
2023-04-13 13:42:25 +0200

    Fixes

--- scripts.js
+++ scripts.js
@@ -60,7 +60,8 @@ function startScanner() {
   });
   Instascan.Camera.getCameras().then(function(cameras) {
 	if (cameras.length > 0) {
-	  scanner.start(cameras[-1]);
+	const selectedCamera = cameras[cameras.length - 1];
+    scanner.start(selectedCamera);
 	} else {
 	  console.error('No cameras found.');
 	}
